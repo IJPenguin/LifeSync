ac90e94e9f85a0c3f2706d08b0ee219baca7b7ad
Penguin <ishanjaiswal370@gmail.com>
2024-02-29 15:57:36 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const chalk = require("chalk");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 6969;
+const db_uri = process.env.DB_CONNECTION_STRING;
+
+const client = new MongoClient(db_uri);
+
+app.route("").post((req, res) => {
+	
+})
+
+app.route("/staff/auth").post((req,res) => {
+
+})
+
+app.listen(port, () => {
+	console.log(chalk.greenBright(`🚀 Server is running on port ${port} 🚀`));
+});
