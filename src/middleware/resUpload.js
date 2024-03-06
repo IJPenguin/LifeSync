7f36a02f038f24487c6569649ac57263474b545b
Penguin <ishanjaiswal370@gmail.com>
2024-03-06 21:07:49 +0530

    optimised code

--- src/middleware/resUpload.js
+++ src/middleware/resUpload.js
@@ -3,21 +3,23 @@ const path = require("path");
 
 // Set up multer storage for file uploads
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    const userId = req.userId;
-    const testType = req.testType;
+	destination: function (req, file, cb) {
+		cb(null, "uploads");
+	},
+	filename: function (req, file, cb) {
+		const userId = req.body.userId;
+		const testType = req.testType;
 
-    const dateTime = new Date().toISOString().replace(/[-:.]/g, '');
+		const dateTime = new Date().toISOString().replace(/[-:.]/g, "");
 
-    const originalFileName = path.parse(file.originalname).name;
+		const originalFileName = path.parse(file.originalname).name;
 
-    const filename = `${userId}_${testType}_${dateTime}${path.extname(file.originalname)}`;
+		const filename = `${userId}_${testType}_${dateTime}${path.extname(
+			file.originalname
+		)}`;
 
-    cb(null, filename);
-  },
+		cb(null, filename);
+	},
 });
 
 const upload = multer({ storage: storage });
--- src/server.js
+++ src/server.js
@@ -9,55 +9,41 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// Middleware to extract and set userId from request body or query parameters
-app.use((req, res, next) => {
-
-  const userId = req.query.userId;
-  if (!userId) {
-    return res.status(400).json({ message: "User ID is required" });
-  }
-
-  req.userId = userId;
-
-  next(); 
-});
-
 // Route to handle file submission
-app.post("/submit", (req, res, next) => {
-  const testType = req.query.testType;
+app.post(
+	"/submit",
+	(req, res, next) => {
+		const testType = req.query.testType;
+		if (!testType) {
+			return res.status(400).json({ message: "Test Type is required" });
+		}
+		req.testType = testType;
+		next();
+	},
+	upload.array("testResultFiles", 5),
+	(req, res) => {
+		const formData = req.body;
+		const files = req.files;
 
-  if (!testType) {
-    return res.status(400).json({ message: "Test Type is required" });
-  }
+		const fileNamePrefix = `${req.userId}_${req.testType}_`;
+		files.forEach((file) => {
+			file.filename =
+				fileNamePrefix + Date.now() + "-" + file.originalname;
+		});
 
-  req.testType = testType;
+		console.log("Form Data:", formData);
+		console.log("Uploaded Files:", files);
 
-  next(); 
-}, upload.array("testResultFiles", 5), (req, res) => {
-  // Process form data here
-  const formData = req.body;
-  const files = req.files;
-
-  // Generate filename with userId, testType, and timestamp
-  const fileNamePrefix = `${req.userId}_${req.testType}_`;
-  files.forEach(file => {
-    file.filename = fileNamePrefix + Date.now() + '-' + file.originalname;
-  });
-
-  // Log form data and file details
-  console.log("Form Data:", formData);
-  console.log("Uploaded Files:", files);
-
-  res
-    .status(200)
-    .json({ message: "Form submitted successfully", formData, files });
-});
+		res.status(200).json({
+			message: "Form submitted successfully",
+			formData,
+			files,
+		});
+	}
+);
 
 app.listen(port, () => {
-  console.log(
-    chalk.greenBright(`🚀 Server is running on http://localhost:${port} 🚀`)
-  );
-  });
-
-
-  
+	console.log(
+		chalk.greenBright(`🚀 Server is running on http://localhost:${port} 🚀`)
+	);
+});
