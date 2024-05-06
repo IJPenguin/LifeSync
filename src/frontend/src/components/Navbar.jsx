7693c0fff7e4c4d993130e07b89c5a51fe7fd3cf
Penguin <ishanjaiswal370@gmail.com>
2024-05-06 22:05:08 +0530

    updated hero section

--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -101,11 +101,11 @@ export default function Navbar() {
 							Reviews
 						</a>
 					</li>
-					<li className="home__nav__list__item home__nav__list__user">
+					{/* <li className="home__nav__list__item home__nav__list__user">
 						<a href="/check" className="home__nav__list__link">
 							Assess Yourself
 						</a>
-					</li>
+					</li> */}
 				</ul>
 			</nav>
 		</div>
